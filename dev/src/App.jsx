32f6e37449d8eff64faf2e35c226df5681e52744
Siraphop Sukchu <gamesiraphop4826@gmail.com>
2024-03-26 22:44:26 +0700

    Init project

---
+++ dev/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+/* Components */
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
