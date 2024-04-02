68f01fb88c9f0f583b4b6b11cd5566eaa525fbb7
Siraphop Sukchu <gamesiraphop4826@gmail.com>
2024-04-02 13:55:57 +0700

    Add hCaptcha

--- dev/src/App.jsx
+++ dev/src/App.jsx
@@ -1,15 +1,36 @@
 import React from 'react'
 import './App.css'
+import HCaptcha from '@hcaptcha/react-hcaptcha';
+import { hcaptchakey } from './config.json'
+import Typography from '@mui/material/Typography';
+import Alert from '@mui/material/Alert';
 
 /* Components */
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import UnderDev from './components/UnderDev'
 
+/* Script */
+import Verify from './script/SimpleCaptcha';
+
 function App() {
   return (
     <>
       <UnderDev />
+      <div id='VerifySuccess' style={{ display: 'none' }}>
+        <Alert className='Cool bounce-in-bottom' style={{ width: '300px', margin: 'auto', borderRadius: '30px' }} variant="filled" severity="success">
+          Verify success , Thank you for testing our auth system!
+        </Alert>
+        <br />
+      </div>
+      <div id='captcha' style={{ textAlign: 'center' }}>
+        <Typography className='Title' variant="h5" gutterBottom>
+          Just a captcha :D
+        </Typography>
+        <HCaptcha sitekey={hcaptchakey} theme='dark' onVerify={Verify} />
+        <br />
+      </div>
+
     </>
   )
 }
---
+++ dev/src/script/SimpleCaptcha.js
@@ -0,0 +1,6 @@
+function Verify() {
+    document.getElementById('captcha').style.display = 'none';
+    document.getElementById('VerifySuccess').style.display = 'block';
+}
+
+export default Verify;
