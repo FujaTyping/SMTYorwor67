291fa77ad8e56fc6d3058ad2b117c1c6fc597a4c
Siraphop Sukchu <gamesiraphop4826@gmail.com>
2024-05-07 15:30:56 +0700

    Rework all

--- dev/src/script/SimpleCaptcha.js
+++
@@ -1,6 +0,0 @@
-function Verify() {
-    document.getElementById('captcha').style.display = 'none';
-    document.getElementById('VerifySuccess').style.display = 'block';
-}
-
-export default Verify;
